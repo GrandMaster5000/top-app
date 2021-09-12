e8bcd2c5dada2d5d627b8ca4e41f9183253e005f
Tolik22869 <ggamer556@mail.ru>
2021-09-12 09:58:44 +0300

    add sort accessibility

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -2,26 +2,39 @@ import { SortEnum, SortProps } from './Sort.props';
 import styles from './Sort.module.css';
 import SortIcon from './sort.svg';
 import classNames from 'classnames';
+import React, { KeyboardEvent} from 'react';
+
 
 export const Sort = ({sort ,setSort ,className, ...props}: SortProps): JSX.Element => {
+  const handleKey = (e: KeyboardEvent, sortType: SortEnum) => {
+    if(e.code == 'Enter' || e.code == 'Space') {
+      e.preventDefault();
+      setSort(sortType);
+    }  
+  };
+
   return (
         <div className={classNames(styles.sort, className)} {...props}>
-            <span
+            <button
             onClick={() => setSort(SortEnum.Raiting)}
+            tabIndex={0}
+            onKeyDown={(e) => handleKey(e, SortEnum.Raiting)}
             className={classNames({
                 [styles.active]: sort == SortEnum.Raiting
             })}
             >
               <SortIcon className={styles.sortIcon} /> По рейтингу
-            </span>
-            <span
+            </button>
+            <button
             onClick={() => setSort(SortEnum.Price)}
+            tabIndex={0}
+            onKeyDown={(e) => handleKey(e, SortEnum.Price)}
             className={classNames({
                 [styles.active]: sort == SortEnum.Price
             })}
             >
               <SortIcon className={styles.sortIcon}/> По цене
-            </span>
+            </button>
         </div>  
   );
 };
